6c3db4cf8461535410a6100045a376eadf18001e
Paweł Sadowski <pawelplsi@gmail.com>
2021-03-07 20:13:21 +0000

    ąąąą

--- car_hunter.py
+++ car_hunter.py
@@ -12,6 +12,8 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 WORD_ID = os.getenv("WORD_ID")
 EXAM_CATEGORY = os.getenv("EXAM_CATEGORY")
+LOGIN = os.getenv("LOGIN")
+PASSWORD = os.getenv("PASSWORD")
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
@@ -25,21 +27,24 @@ def notify(text):
 bestExam = None
 while True:
     try:
-        exams = fetch_exams(WORD_ID, EXAM_CATEGORY, datetime.now())
-        exams_practical = [exam for exam in exams if exam.type == ExamType.PRACTICE]
-        exams_practical.sort(key=lambda exam: exam.date)
-        exam = exams_practical[0]
-        if bestExam != exam:
-            if bestExam is None or exam.date < bestExam.date:
-                notify("NEW BEST EXAM: " + str(exam))
-                print("NEW BEST EXAM: " + str(exam))
-            else:
-                notify("CHUJ: " + str(exam))
-                print("CHUJ: " + str(exam))
-            bestExam = exam
-        for P in exams_practical:
-            print (P)
+        authorization = getAuthorization(LOGIN, PASSWORD)
+        print(f"AUTH = {authorization}")
+        while True:
+            exams = fetch_exams(authorization, WORD_ID, EXAM_CATEGORY, datetime.now())
+            exams_practical = [exam for exam in exams if exam.type == ExamType.PRACTICE]
+            exams_practical.sort(key=lambda exam: exam.date)
+            exam = exams_practical[0]
+            if bestExam != exam:
+                if bestExam is None or exam.date < bestExam.date:
+                    notify("GIT: " + str(exam))
+                    print("GIT: " + str(exam))
+                else:
+                    notify("DUPA: " + str(exam))
+                    print("DUPA: " + str(exam))
+                bestExam = exam
+            for P in exams_practical:
+                print (P)
+            systime.sleep(60)
     except Exception as e:
         print(e)
-
     systime.sleep(60)
--- wordapi.py
+++ wordapi.py
@@ -2,6 +2,7 @@ import json
 from enum import Enum
 from datetime import datetime, date, time
 from dateutil.relativedelta import relativedelta
+from bs4 import BeautifulSoup
 import requests
 
 
@@ -12,7 +13,7 @@ class ExamType(Enum):
 
 class Exam:
     def __init__(self, ID, exam_type, category, word_id, date, places, price):
-        self.ID = ID 
+        self.ID = ID
         self.type = exam_type
         self.category = category
         self.word_id = word_id
@@ -23,7 +24,8 @@ class Exam:
     def __str__(self):
         return str(self.type) + ", cat. " + str(self.category) + \
             ", word=" + str(self.word_id) + ", date=" + str(self.date) + \
-            ", places=" + str(self.places) + ", price=" + str(self.price) + ", id="+str(self.ID)
+            ", places=" + str(self.places) + ", price=" + \
+            str(self.price) + ", id="+str(self.ID)
 
     def __hash__(self):
         return hash(str(self))
@@ -37,11 +39,11 @@ class Exam:
 
 def make_exam(exam_type: ExamType, category: str, word_id: str, examObject):
     return Exam(examObject["id"], exam_type, category, word_id,
-            datetime.strptime(examObject["date"], '%Y-%m-%dT%H:%M:%S'),
+                datetime.strptime(examObject["date"], '%Y-%m-%dT%H:%M:%S'),
                 examObject["places"], examObject["amount"])
 
 
-def fetch_exams(word_id: str, exam_category: str, start_date: datetime, end_date: datetime = None):
+def fetch_exams(authorization: str, word_id: str, exam_category: str, start_date: datetime, end_date: datetime = None):
     if not end_date:
         end_date = start_date + relativedelta(months=1)
 
@@ -55,16 +57,44 @@ def fetch_exams(word_id: str, exam_category: str, start_date: datetime, end_date
     )
 
     response = requests.put(
-        "https://info-car.pl/api/word/word-centers/exam-schedule", data=json.dumps(api_params), headers = {"Content-Type": "application/json"})
+        "https://info-car.pl/api/word/word-centers/exam-schedule", data=json.dumps(api_params), headers={"Content-Type": "application/json", "Authorization": authorization})
+    print(response.request.headers)
     if response.status_code != 200:
         print("HTTP error: code " + str(response.status_code))
+        print(response.headers)
+        return None
 
     exams = list()
     data = json.loads(response.text)
     for day in data["schedule"]["scheduledDays"]:
         for hour in day["scheduledHours"]:
             for theory in hour["theoryExams"]:
-                exams.append(make_exam(ExamType.THEORY, exam_category, word_id, theory))
+                exams.append(make_exam(ExamType.THEORY,
+                                       exam_category, word_id, theory))
             for practice in hour["practiceExams"]:
-                exams.append(make_exam(ExamType.PRACTICE, exam_category, word_id, theory))
+                exams.append(make_exam(ExamType.PRACTICE,
+                                       exam_category, word_id, theory))
     return exams
+
+
+def getLoginCsrf(ses):
+    response = ses.get("https://info-car.pl/oauth2/login")
+    soup = BeautifulSoup(response.text, "html.parser")
+    for in_tag in soup.find_all("form")[0].find_all("input"):
+        if in_tag.attrs.get("name") == "_csrf":
+            return in_tag.attrs.get("value")
+
+
+def getAuthorization(username, password):
+    ses = requests.Session()
+    csrf = getLoginCsrf(ses)
+    res = ses.post("https://info-car.pl/oauth2/login", {"username": username, "password": password, "_csrf": csrf})
+    res = ses.get("https://info-car.pl/oauth2/authorize?response_type=id_token%20token&client_id=client&redirect_uri=https%3A%2F%2Finfo-car.pl%2Fnew%2Fassets%2Frefresh.html&scope=openid%20profile%20email%20resource.read&prompt=none", allow_redirects=False)
+    redirectLoc = res.headers["Location"]
+    START = "https://info-car.pl/new/assets/refresh.html#access_token="
+
+    if(not redirectLoc.startswith(START)):
+        print(f"AUTH redirected to {redirectLoc}")
+        raise "Error returned"
+    token = redirectLoc[len(START):redirectLoc.find("&")]
+    return f"Bearer {token}"
